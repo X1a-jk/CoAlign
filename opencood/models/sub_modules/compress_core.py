f02d9b1f11651c1b69fb38eab7763954081333a3
X1a-jk <13601783348@163.com>
2023-10-12 15:51:06 +0000

    update compression

--- opencood/models/sub_modules/compress_core.py
+++ opencood/models/sub_modules/compress_core.py
@@ -1,15 +1,15 @@
 # compress conv
 import torch.nn as nn
 import spconv
+import torch
 
 class CompressCore(nn.Module):
     def __init__(self, model_cfg, **kwargs):
         super().__init__()
         self.model_cfg = model_cfg
-        print(self.model_cfg)
-        self.out_channel= self.model_cfg['compress_core']['out_channel']
-        self.top_k = self.model_cfg['compress_core']['top_k']
-        self.uniform_r = self.model_cfg['compress_core']['uniform_r']
+        self.out_channel= self.model_cfg['out_channel']
+        self.top_k = self.model_cfg['top_k']
+        self.uniform_r = self.model_cfg['uniform_r']
 
     def forward(self, batch_dict):
         """
@@ -21,7 +21,10 @@ class CompressCore(nn.Module):
                 spatial_features:
 
         """
-        print(batch_dict.keys())
+        voxel_features = batch_dict['spatial_features']
+        info_features = torch.where(voxel_features > 1e-5, 1, 0)
+        print("sparsity: "+str(torch.sum(info_features).item()/info_features.numel()))
+       
         encoded_spconv_tensor = batch_dict['encoded_spconv_tensor']
         spatial_features = encoded_spconv_tensor.dense()
         N, C, D, H, W = spatial_features.shape
