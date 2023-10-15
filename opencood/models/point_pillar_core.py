d80fd35b75e0522cee4e3b423747bcee13c266f7
X1a-jk <13601783348@163.com>
2023-10-15 07:16:03 +0000

    compressor

--- opencood/models/point_pillar_core.py
+++ opencood/models/point_pillar_core.py
@@ -1,32 +1,51 @@
 import torch.nn as nn
-
+import numpy as np
 from opencood.models.sub_modules.pillar_vfe import PillarVFE
 from opencood.models.sub_modules.point_pillar_scatter import PointPillarScatter
 from opencood.models.sub_modules.base_bev_backbone import BaseBEVBackbone
 from opencood.models.sub_modules.base_bev_backbone_resnet import ResNetBEVBackbone
 from opencood.models.sub_modules.downsample_conv import DownsampleConv
+from opencood.models.sub_modules.compress_core import CompressCore
 from opencood.models.sub_modules.naive_compress import NaiveCompressor
-from opencood.models.sub_modules.dcn_net import DCNNet
+# from opencood.models.sub_modules.dcn_net import DCNNet
 # from opencood.models.fuse_modules.where2comm import Where2comm
 from opencood.models.fuse_modules.where2comm_attn import Where2comm
 import torch
+from functools import partial
 
-class PointPillarCore(nn.Module):
+class pointpillarcore(nn.Module):
     def __init__(self, args):
-        super(PointPillarCore, self).__init__()
-
+        super(pointpillarcore, self).__init__()
         # PIllar VFE
         self.pillar_vfe = PillarVFE(args['pillar_vfe'],
                                     num_point_features=4,
                                     voxel_size=args['voxel_size'],
                                     point_cloud_range=args['lidar_range'])
         self.scatter = PointPillarScatter(args['point_pillar_scatter'])
+        
+        
         if 'resnet' in args['base_bev_backbone']:
             self.backbone = ResNetBEVBackbone(args['base_bev_backbone'], 64)
         else:
             self.backbone = BaseBEVBackbone(args['base_bev_backbone'], 64)
-        self.out_channel = sum(args['base_bev_backbone']['num_upsample_filter'])
 
+        self.voxel_size = args['voxel_size']
+        self.out_size_factor = args['out_size_factor']
+        self.cav_lidar_range  = args['lidar_range']
+
+        grid_size = (np.array(self.cav_lidar_range[3:6]) -
+                     np.array(self.cav_lidar_range[0:3])) / np.array(self.voxel_size)
+        self.grid_size = np.round(grid_size).astype(np.int64)
+        norm_fn = partial(nn.BatchNorm2d, eps=1e-3, momentum=0.01)
+        self.conv = nn.Sequential(
+            nn.Conv2d(args['compress_core']['in_channels'], args['compress_core']['out_channels'], kernel_size=1, stride=1, padding=0, bias=False),
+            norm_fn(args['compress_core']['out_channels']),
+            nn.ReLU(),
+        )
+
+        self.compressor = CompressCore(args['compress_core'], self.conv)
+
+        self.out_channel = sum(args['base_bev_backbone']['num_upsample_filter'])
 
         # used to downsample the feature map for efficient computation
         self.shrink_flag = False
@@ -34,31 +53,34 @@ class PointPillarCore(nn.Module):
             self.shrink_flag = True
             self.shrink_conv = DownsampleConv(args['shrink_header'])
             self.out_channel = args['shrink_header']['dim'][-1]
+        
         self.compression = False
-
         if 'compression' in args and args['compression'] > 0:
             self.compression = True
-            self.naive_compressor = NaiveCompressor(256, args['compression'])
+            self.naive_compressor = NaiveCompressor(self.out_channel, args['compression'])
+
+        self.dcn = False
+        if 'dcn' in args:
+            self.dcn = True
+            self.dcn_net = DCNNet(args['dcn'])
 
         # self.fusion_net = TransformerFusion(args['fusion_args'])
-        
-        
-        # self.fusion_net = Where2comm(args['fusion_args'])
-        
-        
+        self.fusion_net = Where2comm(args['fusion_args'])
         self.multi_scale = args['fusion_args']['multi_scale']
 
         self.cls_head = nn.Conv2d(self.out_channel, args['anchor_number'],
                                   kernel_size=1)
-        self.reg_head = nn.Conv2d(self.out_channel, 7 * args['anchor_number'],
+        self.reg_head = nn.Conv2d(self.out_channel, 8 * args['anchor_number'],
                                   kernel_size=1)
-        self.use_dir = False
-        if 'dir_args' in args.keys():
-            self.use_dir = True
-            self.dir_head = nn.Conv2d(self.out_channel, args['dir_args']['num_bins'] * args['anchor_number'],
-                                  kernel_size=1) # BIN_NUM = 2
-        if "backbone_fix" in args and args['backbone_fix']:
+        if 'backbone_fix' in args.keys() and args['backbone_fix']:
             self.backbone_fix()
+        
+        self.init_weight()
+    
+    def init_weight(self):
+        pi = 0.01
+        nn.init.constant_(self.cls_head.bias, -np.log((1 - pi) / pi) )
+        nn.init.normal_(self.reg_head.weight, mean=0, std=0.001)
 
     def backbone_fix(self):
         """
@@ -91,11 +113,11 @@ class PointPillarCore(nn.Module):
         return split_x
 
     def forward(self, data_dict):
-        voxel_features = data_dict['processed_lidar']['voxel_features']
-        voxel_coords = data_dict['processed_lidar']['voxel_coords']
-        voxel_num_points = data_dict['processed_lidar']['voxel_num_points']
+        voxel_features = data_dict['processed_lidar']['voxel_features'] # (34814,32,4)
+        voxel_coords = data_dict['processed_lidar']['voxel_coords']  # (34814,4)
+        voxel_num_points = data_dict['processed_lidar']['voxel_num_points'] # (34814)
         record_len = data_dict['record_len']
-
+        
         pairwise_t_matrix = data_dict['pairwise_t_matrix']
 
         batch_dict = {'voxel_features': voxel_features,
@@ -106,23 +128,31 @@ class PointPillarCore(nn.Module):
         batch_dict = self.pillar_vfe(batch_dict)
         # n, c -> N, C, H, W
         batch_dict = self.scatter(batch_dict)
+        
         batch_dict = self.backbone(batch_dict)
         # N, C, H', W'. [N, 384, 100, 352]
+
+        batch_dict = self.compressor(batch_dict)
         spatial_features_2d = batch_dict['spatial_features_2d']
-        
+            
         # downsample feature to reduce memory
         if self.shrink_flag:
             spatial_features_2d = self.shrink_conv(spatial_features_2d)
         # compressor
         if self.compression:
             spatial_features_2d = self.naive_compressor(spatial_features_2d)
+        # dcn
+        if self.dcn:
+            spatial_features_2d = self.dcn_net(spatial_features_2d)
         # spatial_features_2d is [sum(cav_num), 256, 50, 176]
         # output only contains ego
         # [B, 256, 50, 176]
         psm_single = self.cls_head(spatial_features_2d)
         rm_single = self.reg_head(spatial_features_2d)
-        if self.use_dir:
-            dir_single = self.dir_head(spatial_features_2d)
+
+        # print('spatial_features_2d: ', spatial_features_2d.shape)
+
+        print("ready to fuse")
 
         if self.multi_scale:
             fused_feature, communication_rates, result_dict = self.fusion_net(batch_dict['spatial_features'],
@@ -141,20 +171,84 @@ class PointPillarCore(nn.Module):
             
             
         # print('fused_feature: ', fused_feature.shape)
-        psm = self.cls_head(fused_feature)
-        rm = self.reg_head(fused_feature)
-        
+        cls = self.cls_head(fused_feature) # fused_feature [12, 128, 96, 288] -> [12, 3, 96, 288]
+        bbox = self.reg_head(fused_feature) # fused_feature [12, 128, 96, 288] -> [12, 24, 96, 288]
 
-        output_dict = {'cls_preds': psm,
-                       'reg_preds': rm}
-        if self.use_dir:
-            output_dict.update({'dir_preds': self.dir_head(fused_feature),
-                                'dir_preds_single': dir_single})
+        box_preds_for_infer = bbox.permute(0, 2, 3, 1).contiguous()
+        bbox_temp_list = []
+        num_class = int(box_preds_for_infer.shape[3]/8)
+        box_preds_for_infer = box_preds_for_infer.view(box_preds_for_infer.shape[0], box_preds_for_infer.shape[1], box_preds_for_infer.shape[2], num_class, 8)
+        for i in range(num_class): # num_class
+            box_preds_for_infer_singleclass = box_preds_for_infer[:,:,:,i,:]
+            box_preds_for_infer_singleclass = box_preds_for_infer_singleclass.permute(0, 3, 1, 2)
+            _, bbox_temp = self.generate_predicted_boxes(cls[:, i, :, :], box_preds_for_infer_singleclass)
+            bbox_temp_list.append(bbox_temp)
+        bbox_temp_list = torch.stack(bbox_temp_list, dim=1)
 
+
+        _, bbox_temp = self.generate_predicted_boxes(cls, bbox)
+
+        output_dict = {'cls_preds': cls, # (4,1,100,100) -> [1, 3, 92, 92]
+                       'reg_preds': bbox_temp, #(4,10000,7)
+                       'reg_preds_multiclass': bbox_temp_list, # [1, 3, 27648, 7]
+                       'bbox_preds': bbox #(4,8,100,100) -> [1, 24, 92, 92]
+                       }
         output_dict.update(result_dict)
 
-        output_dict.update({'cls_preds_single': psm_single,
-                       'reg_preds_single': rm_single,
+        _, bbox_temp_single = self.generate_predicted_boxes(psm_single, rm_single)
+
+        output_dict.update({'cls_preds_single': psm_single, # [12, 1, 100, 100]
+                       'reg_preds_single': bbox_temp_single,  # [12, 10000, 7]
+                       'bbox_preds_single': rm_single,   # [12, 8, 100, 100]
                        'comm_rate': communication_rates
                        })
         return output_dict
+
+    def generate_predicted_boxes(self, cls_preds, box_preds, dir_cls_preds=None):
+        """
+        Args:
+            batch_size:
+            cls_preds: (N, H, W, C1)
+            box_preds: (N, H, W, C2)
+            dir_cls_preds: (N, H, W, C3)
+
+        Returns:
+            batch_cls_preds: (B, num_boxes, num_classes)
+            batch_box_preds: (B, num_boxes, 7+C)
+
+        """
+        box_preds = box_preds.permute(0, 2, 3, 1).contiguous()
+        
+        batch, H, W, code_size = box_preds.size()   ## code_size 表示的是预测的尺寸
+        
+        box_preds = box_preds.reshape(batch, H*W, code_size)
+
+        batch_reg = box_preds[..., 0:2]
+        # batch_hei = box_preds[..., 2:3] 
+        # batch_dim = torch.exp(box_preds[..., 3:6])
+        
+        h = box_preds[..., 3:4] * self.out_size_factor * self.voxel_size[0]
+        w = box_preds[..., 4:5] * self.out_size_factor * self.voxel_size[1]
+        l = box_preds[..., 5:6] * self.out_size_factor * self.voxel_size[2]
+        batch_dim = torch.cat([h,w,l], dim=-1)
+        batch_hei = box_preds[..., 2:3] * self.out_size_factor * self.voxel_size[2] + self.cav_lidar_range[2]
+
+        batch_rots = box_preds[..., 6:7]
+        batch_rotc = box_preds[..., 7:8]
+
+        rot = torch.atan2(batch_rots, batch_rotc)
+
+        ys, xs = torch.meshgrid([torch.arange(0, H), torch.arange(0, W)])
+        ys = ys.view(1, H, W).repeat(batch, 1, 1).to(cls_preds.device)
+        xs = xs.view(1, H, W).repeat(batch, 1, 1).to(cls_preds.device)
+
+        xs = xs.view(batch, -1, 1) + batch_reg[:, :, 0:1]
+        ys = ys.view(batch, -1, 1) + batch_reg[:, :, 1:2]
+
+        xs = xs * self.out_size_factor * self.voxel_size[0] + self.cav_lidar_range[0]   ## 基于feature_map 的size求解真实的坐标
+        ys = ys * self.out_size_factor * self.voxel_size[1] + self.cav_lidar_range[1]
+
+
+        batch_box_preds = torch.cat([xs, ys, batch_hei, batch_dim, rot], dim=2)
+
+        return cls_preds, batch_box_preds
--- opencood/models/sub_modules/compress_core.py
+++ opencood/models/sub_modules/compress_core.py
@@ -2,14 +2,21 @@
 import torch.nn as nn
 import spconv
 import torch
+from opencood.utils import spconv_utils
+from functools import partial
+
 
 class CompressCore(nn.Module):
-    def __init__(self, model_cfg, **kwargs):
+    def __init__(self, model_cfg, compressor, **kwargs):
         super().__init__()
         self.model_cfg = model_cfg
-        self.out_channel= self.model_cfg['out_channel']
+        #self.in_channels = self.model_cfg['in_channels']
+        #self.out_channels = self.model_cfg['out_channels']
         self.top_k = self.model_cfg['top_k']
         self.uniform_r = self.model_cfg['uniform_r']
+        #self.sparse_shape = grid_size[::-1] + [1, 0, 0]
+        norm_fn = partial(nn.BatchNorm2d, eps=1e-3, momentum=0.01)
+        self.conv = compressor
 
     def forward(self, batch_dict):
         """
@@ -21,10 +28,57 @@ class CompressCore(nn.Module):
                 spatial_features:
 
         """
-        voxel_features = batch_dict['spatial_features']
-        info_features = torch.where(voxel_features > 1e-5, 1, 0)
-        print("sparsity: "+str(torch.sum(info_features).item()/info_features.numel()))
-       
+        #print(batch_dict['record_len'])
+        #voxel_features, voxel_coords = batch_dict['voxel_features'], batch_dict['voxel_coords']
+        #batch_size = 1 # 应该是batch_dict['batch_size'] 之后有问题再看
+        
+        
+        N, C, H, W = batch_dict['spatial_features'].shape
+        features = batch_dict['spatial_features']
+        
+        '''
+        input_sp_tensor = spconv.SparseConvTensor(
+            features=voxel_features, 
+            indices=voxel_coords.int(), 
+            spatial_shape=self.sparse_shape, 
+            batch_size=N
+        )
+
+        assert C == self.in_channels
+        print(input_sp_tensor.dense())
+        '''
+        compressed = self.conv(features)
+        compressed = torch.sum(compressed, dim=1)
+        features_selected = int(H * W * self.top_k)
+        compressed_flattened = compressed.flatten(1)
+        values, indices = compressed_flattened.topk(features_selected, dim=1, largest=True)
+        new_features_selected = int(features_selected * self.uniform_r)
+        '''
+        select = torch.Tensor(N, features_selected)
+        select = select.uniform_(0, features_selected - 1).int()
+        indices = indices[select]
+        '''
+        idx = torch.randperm(N)
+        indices = indices[idx][:,:new_features_selected]
+        
+        sparse_features = torch.zeros((N,new_features_selected))
+        sparse_indices = torch.zeros((N,new_features_selected,2), dtype=torch.int)
+        for i in range(N):
+            index_temp = indices[i]
+            for j in range(len(index_temp)):
+                coords = index_temp[j]
+                h_temp = coords // W
+                w_temp = coords % W
+                
+                sparse_indices[i,j,0]=h_temp
+                sparse_indices[i,j,1]=w_temp
+
+                sparse_features[i,j]=compressed[i,h_temp,w_temp]
+
+        batch_dict['core_features'] = sparse_features
+        batch_dict['core_indices'] = sparse_indices
+        batch_dict['compressed_2d_feature'] = compressed
+        '''
         encoded_spconv_tensor = batch_dict['encoded_spconv_tensor']
         spatial_features = encoded_spconv_tensor.dense()
         N, C, D, H, W = spatial_features.shape
@@ -32,4 +86,7 @@ class CompressCore(nn.Module):
         batch_dict['spatial_features'] = spatial_features
         batch_dict['spatial_features_stride'] = \
             batch_dict['encoded_spconv_tensor_stride']
+        '''
+        #print(sparse_features)
+        #print(sparse_indices)
         return batch_dict
